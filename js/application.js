040cef6d81d91526e324395dea90db9a0dbc350c
weijefffrey <jeffreycswei@gmail.com>
2015-07-14 22:43:29 +0800

    fixed dblclick

--- js/application.js
+++ js/application.js
@@ -68,11 +68,24 @@ var setBranch = function(row, col){
     branchPos = [row,col]; 
 };
 
-//clear position and clear screen
-var clearPosition = function(row, col){
-    if(row<frame.length-1){
-    frame[row][col] = 0;
+var clearBranch = function(row, col){
+    if((row<game.frame.length) && (col === 0)){
+        game.frame[row][col] = 0;
+        game.frame[row][col+1] = 0;
+    } else{
+        game.frame[row][col+1] = 0;
     }
+};
+
+//clear position and clear screen
+var clearPig = function(){
+    pigCol = null;
+    
+};
+
+var clearMonster = function(){
+    monsterCol = null;
+    
 };
 
 var clearScreen = function(row, col){
@@ -94,23 +107,11 @@ var countDown = setInterval(function(){
     if(timeLeft <= 0){clearInterval(countDown)}
 },1000)
 
-//test set up
-var test = function(){
-    setMonster(3);
-    setBranch(1,2);
-    setBranch(1,3);
-    setBranch(3,2);
-    setBranch(3,3);
-    setBranch(6,2);
-    setBranch(6,3);
-    setPig(2);
-    log(game.frame);
-};
 
 // movement
 var checkRightSpot = function(){
     if((monsterCol<colNum-1)&&(game.frame[monsterRow][monsterCol+1] !== 0)&&(game.frame[monsterRow][monsterCol+1] !== 9)){
-    game.frame[monsterRow][monsterCol+1] = monster;
+    game.frame[monsterRow][monsterCol+1] = game.monster;
     monsterCol++;
     log(game.frame);
     log('right');
@@ -121,7 +122,7 @@ var checkRightSpot = function(){
 
 var checkLeftSpot = function(){
     if((monsterCol-1>=0)&&(game.frame[monsterRow][monsterCol-1] !== 0)&&(game.frame[monsterRow][monsterCol-1] !== 9)){
-    game.frame[monsterRow][monsterCol-1] = monster;
+    game.frame[monsterRow][monsterCol-1] = game.monster;
     monsterCol--;
     log(game.frame)
     log('left')
@@ -133,13 +134,17 @@ var checkLeftSpot = function(){
 var checkDownSpot = function(){
     if(monsterRow<game.frame.length){
     monsterRow++;
-    game.frame[monsterRow][monsterCol] = monster;
+    game.frame[monsterRow][monsterCol] = game.monster;
     log(game.frame)
     log('down')
     return true
 } 
     return false
 }   ; 
+
+var initMove = function(){
+    game.frame[0][monsterCol] = game.monster
+}
 
 var move = function(){
         if(checkRightSpot() === true){
@@ -160,197 +165,345 @@ var winner = function(){
     
 }
 
-
-
 $(document).ready(function(){
-// colNum = parseInt($('.numOfColumn').val());
-// colWidth = 700/(colNum);
+
+//set branches in graphics
+chooseGroup = function(){
+        switch(group) {
+        case 'group1':
+            setBranch(0,0)
+            break;
+        case 'group2':
+            setBranch(1,0)
+            break;
+        case 'group3':
+            setBranch(2,0)
+            break;
+        case 'group4':
+            setBranch(3,0)
+            break;
+        case 'group5':
+            setBranch(4,0)
+            break;
+        case 'group6':
+            setBranch(5,0)
+            break;
+        case 'group7':
+            setBranch(6,0)
+            break;
+        case 'group8':
+            setBranch(7,0)
+            break;
+
+        case 'group9':
+            setBranch(0,1)
+            break;
+        case 'group10':
+            setBranch(1,1)
+            break;
+        case 'group11':
+            setBranch(2,1)
+            break;
+        case 'group12':
+            setBranch(3,1)
+            break;
+        case 'group13':
+            setBranch(4,1)
+            break;
+        case 'group14':
+            setBranch(5,1)
+            break;
+        case 'group15':
+            setBranch(6,1)
+            break;
+        case 'group16':
+            setBranch(7,1)
+            break;
+
+        case 'group17':
+        setBranch(0,2)
+            break;
+        case 'group18':
+        setBranch(1,2)
+            break;
+        case 'group19':
+        setBranch(2,2)
+            break;
+        case 'group20':
+        setBranch(3,2)
+            break;
+        case 'group21':
+        setBranch(4,2)
+            break;
+        case 'group22':
+        setBranch(5,2)
+            break;
+        case 'group23':
+        setBranch(6,2)
+            break;
+        case 'group24':
+        setBranch(7,2)
+            break;
+
+        case 'group25':
+        setBranch(0,3)
+            break;
+        case 'group26':
+        setBranch(1,3)
+            break;
+        case 'group27':
+        setBranch(2,3)
+            break;
+        case 'group28':
+        setBranch(3,3)
+            break;
+        case 'group29':
+        setBranch(4,3)
+            break;
+        case 'group30':
+        setBranch(5,3)
+            break;
+        case 'group31':
+        setBranch(6,3)
+            break;
+        case 'group32':
+        setBranch(7,3)
+            break;
+        default:
+            console.log('default');
+        }
+};
+
+   
+//erase branches
+clearGroup = function(){
+        switch(group) {
+        case 'group1':
+            clearBranch(0,0)
+            break;
+        case 'group2':
+            clearBranch(1,0)
+            break;
+        case 'group3':
+            clearBranch(2,0)
+            break;
+        case 'group4':
+            clearBranch(3,0)
+            break;
+        case 'group5':
+            clearBranch(4,0)
+            break;
+        case 'group6':
+            clearBranch(5,0)
+            break;
+        case 'group7':
+            clearBranch(6,0)
+            break;
+        case 'group8':
+            clearBranch(7,0)
+            break;
+
+        case 'group9':
+            clearBranch(0,1)
+            break;
+        case 'group10':
+            clearBranch(1,1)
+            break;
+        case 'group11':
+            clearBranch(2,1)
+            break;
+        case 'group12':
+            clearBranch(3,1)
+            break;
+        case 'group13':
+            clearBranch(4,1)
+            break;
+        case 'group14':
+            clearBranch(5,1)
+            break;
+        case 'group15':
+            clearBranch(6,1)
+            break;
+        case 'group16':
+            clearBranch(7,1)
+            break;
+
+        case 'group17':
+        clearBranch(0,2)
+            break;
+        case 'group18':
+        clearBranch(1,2)
+            break;
+        case 'group19':
+        clearBranch(2,2)
+            break;
+        case 'group20':
+        clearBranch(3,2)
+            break;
+        case 'group21':
+        clearBranch(4,2)
+            break;
+        case 'group22':
+        clearBranch(5,2)
+            break;
+        case 'group23':
+        clearBranch(6,2)
+            break;
+        case 'group24':
+        clearBranch(7,2)
+            break;
+
+        case 'group25':
+        clearBranch(0,3)
+            break;
+        case 'group26':
+        clearBranch(1,3)
+            break;
+        case 'group27':
+        clearBranch(2,3)
+            break;
+        case 'group28':
+        clearBranch(3,3)
+            break;
+        case 'group29':
+        clearBranch(4,3)
+            break;
+        case 'group30':
+        clearBranch(5,3)
+            break;
+        case 'group31':
+        clearBranch(6,3)
+            break;
+        case 'group32':
+        clearBranch(7,3)
+            break;
+        default:
+            console.log('default');
+        }
+};
 
 
-
-$(document).on('click', '.indiBranch', function(){
-    console.log("indiBranch");
+drawBranch = function(){
+    console.log("drawBranch");
     $(this).css('stroke', 'rgba(139,69,19,1)')
+    console.log($(this).prop('id'))
+    group = $(this).prop('id');
+    chooseGroup(group);
+    $(this).one("click", deleteBranch)
+};
 
-});
-
-$(document).on('dblclick', '.indiBranch', function(){
-    console.log("indiBranch");
+deleteBranch = function(){
+    console.log("deleteBranch");
     $(this).css('stroke', 'rgba(139,69,19,0)')
+    console.log($(this).prop('id'))
+    group = $(this).prop('id');
+    clearGroup(group);
+    $(this).one("click", drawBranch)
+};
+
+$(".indiBranch").one("click", drawBranch);
+ 
+
+//set the Pig
+whichPig = function(){
+        switch(piggy) {
+        case 'pig0':
+            setPig(0)
+            break;
+        case 'pig1':
+            setPig(1)
+            break;
+        case 'pig2':
+            setPig(2)
+            break;
+        case 'pig3':
+            setPig(3)
+            break;
+        case 'pig4':
+            setPig(4)
+            break;
+        default:
+            console.log('default');
+        }
+};
+
+drawPig = function(){
+    console.log("drawPig");
+    $(this).css('fill', 'rgba(34,139,34,0)')
+    console.log($(this).prop('id'))
+    piggy = $(this).prop('id');
+    whichPig(piggy);
+    $(this).one("click", deletePig)
+};
+
+//erase pig position
+deletePig = function(){
+    console.log("deletePig");
+    $(this).css('fill', 'rgba(34,139,34,1)')
+    console.log($(this).prop('id'))
+    group = $(this).prop('id');
+    clearPig();
+    $(this).one("click", drawPig)
+};
+
+$(".pigCover").one("click", drawPig)
+
+
+//set the Monster
+whichMonster = function(){
+        switch(monster) {
+        case 'monster0':
+            setMonster(0)
+            break;
+        case 'monster1':
+            setMonster(1)
+            break;
+        case 'monster2':
+            setMonster(2)
+            break;
+        case 'monster3':
+            setMonster(3)
+            break;
+        case 'monster4':
+            setMonster(4)
+            break;
+        default:
+            console.log('default');
+        }
+};
+
+drawMonster = function(){
+    log("drawMonster");
+    $(this).css('fill', 'rgba(100,149,237,0)')
+    log($(this).prop('id'))
+    monster = $(this).prop('id');
+    whichMonster(monster);
+    $(this).one("click", deleteMonster)
+};
+
+
+//erase monster position
+deleteMonster = function(){
+    log("deleteMonster");
+    $(this).css('fill', 'rgba(100,149,237,1)')
+    log($(this).prop('id'))
+    group = $(this).prop('id');
+    clearMonster();
+    $(this).one("click", drawMonster)
+};
+
+$(".monsterCover").one("click", drawMonster)
+//end of monster drawing/delete
+
+// monster go and eat
+$(".monsterButton").click(function(){
+  
+
+})
+
+
+
 
 });
+//end of jQuery
 
-
-        
-//draw background
-    $('.go').click(function(){
-      console.log("go button clicked")
-      
-
-    })
-
- 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-})//end of jQuery
-
-// branchCoordinatesX =[];
-// branchCoordinatesY =[];
-
-
-// // canvas drawing
-// background = function(){
-// colNum = parseInt($('.numOfColumn').val());
-// colWidth = 700/(colNum);
-
-//   canvas = document.getElementById("playScreen")
-//   ctx = canvas.getContext('2d');
-//   canvas.setAttribute('width', '800');
-//   canvas.setAttribute('height', '600');
-
-
-//   ctx.fillStyle = "#6495ED";
-//   sky = new Path2D();
-//   ctx.fillRect(0,0,800,200);
-
-//   ctx.fillStyle = "#228b22";
-//   grass = new Path2D();
-//   ctx.fillRect(0,200,800,400);
-
-//   cloud = new Path2D();
-//   ctx.lineWidth = 3;
-//   ctx.beginPath();
-//   ctx.moveTo(45, 139);
-//   ctx.quadraticCurveTo(61,97,89,110);  
-//   ctx.quadraticCurveTo(105,58,127,110);
-//   ctx.quadraticCurveTo(165,94,180,139);
-//   ctx.closePath(); 
-//   ctx.fillStyle = '#f8f8ff';
-//   ctx.fill();
-//   ctx.strokeStyle = '#f8f8ff';
-//   ctx.stroke();
-
-//   cloud2 = new Path2D();
-//   ctx.lineWidth = 3;
-//   ctx.beginPath();
-//   ctx.moveTo(200, 115);
-//   ctx.quadraticCurveTo(216,72,244,86);  
-//   ctx.quadraticCurveTo(260,34,282,86);
-//   ctx.quadraticCurveTo(320,70,335,115);
-//   ctx.closePath(); 
-//   ctx.fillStyle = '#f8f8ff';
-//   ctx.fill();
-//   ctx.strokeStyle = '#f8f8ff';
-//   ctx.stroke();
-
-//   cloud3 = new Path2D();
-//   ctx.lineWidth = 3;
-//   ctx.beginPath();
-//   ctx.moveTo(360, 165);
-//   ctx.quadraticCurveTo(376,122,404,136);  
-//   ctx.quadraticCurveTo(420,84,432,136);
-//   ctx.quadraticCurveTo(480,120,495,165);
-//   ctx.closePath(); 
-//   ctx.fillStyle = '#f8f8ff';
-//   ctx.fill();
-//   ctx.strokeStyle = '#f8f8ff';
-//   ctx.stroke();
-
-//   cloud4 = new Path2D();
-//   ctx.lineWidth = 3;
-//   ctx.beginPath();
-//   ctx.moveTo(520, 100);
-//   ctx.quadraticCurveTo(536,57,564,71);  
-//   ctx.quadraticCurveTo(580,40,592,71);
-//   ctx.quadraticCurveTo(640,55,670,100);
-//   ctx.closePath(); 
-//   ctx.fillStyle = '#f8f8ff';
-//   ctx.fill();
-//   ctx.strokeStyle = '#f8f8ff';
-//   ctx.stroke();
-
-// //maze
-//   columns = new Path2D();
-//     for(i=0; i<colNum; i++){
-//       ctx.lineWidth = 14;
-//       ctx.lineCap ='round';
-//       ctx.beginPath();
-//       ctx.moveTo(600/(colNum)+(i*colWidth), 240);
-//       ctx.lineTo(600/(colNum)+(i*colWidth), 540);
-//       ctx.strokeStyle = '#90ee90'
-//       ctx.stroke();  
-//     };
-
-//   wayPoints = new Path2D();
-//   for(k=0; k<colNum; k++){
-//     for (j=0; j<game.frame.length; j++){
-//     ctx.lineWidth = 5;
-//     ctx.lineCap = 'round';
-//     ctx.beginPath();
-//     ctx.moveTo(600/(colNum)+(k*colWidth), 240+(j*(300/(game.frame.length-1))));
-//     ctx.lineTo(600/(colNum)+(k*colWidth), 240.1+(j*(300/(game.frame.length-1))));
-//     ctx.strokeStyle = '#d2691e';
-//     ctx.stroke();
-//     console.log(600/(colNum)+(k*colWidth), 240+(j*(300/(game.frame.length-1))))      
-//     branchCoordinatesX.push(600/(colNum)+(k*colWidth));
-//     branchCoordinatesY.push(240+(j*(300/(game.frame.length-1))));
-//     }}
-
-// };// end of canvas drawing (background fucntion())
-
-
-// //draw branch
-
-// getMousePos = function(e){
-//     mx = e.clientX - playScreen.getBoundingClientRect().left; 
-//     my = e.clientY - playScreen.getBoundingClientRect().top; 
-//     console.log(mx, my)
-
-// }
-
-
-// $('#playScreen').click(function(e){
-//   console.log("canvas clicked");
-//   getMousePos(e);
-//   for (i=0; i<branchCoordinatesX.length; i++){
-//     for (j=0; j<branchCoordinatesY.length; j++){
-//         if((mx == Math.round((branchCoordinatesX[i]/10)*10)) && (my == Math.round((branchCoordinatesY[j]/10)*10)) && (my !== branchCoordinatesX[branchCoordinatesX-1])){
-//         branches();
-//         } else {
-//             console.log ("cannot set up branch")
-//         }
-//     }
-//   }
-//   });
-
-
-//     branches = function(){
-//     canvas = document.getElementById("playScreen")
-//     ctx = canvas.getContext('2d');
-
-//     branch = new Path2D();
-//     ctx.lineWidth = 10;
-//     ctx.lineCap = 'round';
-//     ctx.beginPath();
-//     ctx.moveTo(mx, my);
-//     ctx.lineTo(mx+colWidth, my);
-//     ctx.strokeStyle = '#8b4513';
-//     ctx.stroke();
-// }
-
-
-
-
-
-  
